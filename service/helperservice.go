9fe24874b0d4921b68629d9d025df74c9ff7c183
amanullah68 <aman.zahoor68@gmail.com>
2024-09-13 08:50:16 +0400

    fix: Activated operators struct

--- service/helperservice.go
+++ service/helperservice.go
@@ -2,51 +2,69 @@ package service
 
 import (
 	"context"
-	"fmt"
-	"github.com/machinebox/graphql"
-	"github.com/tokamak-network/DRB-node/logger"
-	"github.com/tokamak-network/DRB-node/utils"
+	"log"
 	"strings"
+
+	"github.com/machinebox/graphql"
+	"github.com/tokamak-network/DRB-node/utils"
 )
 
 func IsOperator(operator string) (bool, error) {
 	config := utils.GetConfig()
 	client := graphql.NewClient(config.SubgraphURL)
 
+	// Get GraphQL query request
 	req := utils.GetIsOperatorRequest()
+	req.Header.Set("Content-Type", "application/json")
 
+	// Define the response struct
 	var respData struct {
-		Data struct {
-			ActivatedOperatorsCollection []struct {
-				Operators      []string `json:"operators"`
-				OperatorsCount string   `json:"operatorsCount"`
-			} `json:"activatedOperators_collection"`
-			ActivatedOperators struct {
-				Operators      []string `json:"operators"`
-				OperatorsCount string   `json:"operatorsCount"`
-			} `json:"activatedOperators"`
-		} `json:"data"`
+		ActivatedOperatorsCollection []struct {
+			Operators      []string `json:"operators"`
+			OperatorsCount string   `json:"operatorsCount"`
+		} `json:"activatedOperators_collection"`
+		ActivatedOperators struct {
+			Operators      []string `json:"operators"`
+			OperatorsCount string   `json:"operatorsCount"`
+		} `json:"activatedOperators"`
 	}
 
-	fmt.Println("req: ", req)
+	// Execute the query
 	ctx := context.Background()
-	if err := client.Run(ctx, req, &respData); err != nil {
-		logger.Log.Errorf("Failed to execute query: %v", err)
+	err := client.Run(ctx, req, &respData)
+	if err != nil {
+		log.Printf("GraphQL query failed with error: %v", err)
 		return false, err
 	}
 
-	logger.Log.Infof("Activated Operators Collection Data: %+v", respData.Data.ActivatedOperatorsCollection)
-	logger.Log.Infof("Activated Operators Data: %+v", respData.Data.ActivatedOperators)
+	// Log the raw response data
+	log.Printf("Raw GraphQL Response: %+v\n", respData)
 
+	// Check if data is populated for both collections
+	if len(respData.ActivatedOperatorsCollection) == 0 {
+		log.Printf("No operators received in activatedOperators_collection")
+	} else {
+		for _, collection := range respData.ActivatedOperatorsCollection {
+			log.Printf("Operators received in activatedOperators_collection: %+v", collection.Operators)
+		}
+	}
+
+	if len(respData.ActivatedOperators.Operators) == 0 {
+		log.Printf("No operators received in activatedOperators")
+	} else {
+		log.Printf("Operators received in activatedOperators: %+v", respData.ActivatedOperators.Operators)
+	}
+
+	// Determine if the operator exists in the `activatedOperators` list
 	isOperator := false
-	for _, op := range respData.Data.ActivatedOperators.Operators {
+	for _, op := range respData.ActivatedOperators.Operators {
 		if strings.ToLower(op) == strings.ToLower(operator) {
 			isOperator = true
 			break
 		}
 	}
 
-	logger.Log.Infof("Operator status for %s: %v", operator, isOperator)
+	log.Printf("Is operator %s: %v", operator, isOperator)
 
 	return isOperator, nil
 }
