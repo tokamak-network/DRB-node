bc7be1aedd700d2590714a509f3aa289e3b44b2a
heun630 <egg6030@gmail.com>
2024-09-13 11:05:47 +0900

    feat: deposit and activate

--- utils/graphql_queries.go
+++ utils/graphql_queries.go
@@ -3,7 +3,7 @@ package utils
 import "github.com/machinebox/graphql"
 
 const (
-    RandomWordsRequestedQuery = `
+	RandomWordsRequestedQuery = `
         query MyQuery {
             randomWordsRequesteds(orderBy: blockTimestamp, orderDirection: desc, first: 50) {
                 blockTimestamp
@@ -17,7 +17,7 @@ const (
             }
         }`
 
-    CommitCsQuery = `
+	CommitCsQuery = `
         query MyQuery($round: String!, $msgSender: String!) {
             commitCs(where: {round: $round, msgSender: $msgSender}) {
                 blockTimestamp
@@ -25,7 +25,7 @@ const (
             }
         }`
 
-    RecoveredDataQuery = `
+	RecoveredDataQuery = `
         query MyQuery($round: String!) {
             recovereds(orderBy: blockTimestamp, orderDirection: asc, where: {round: $round}) {
                 round
@@ -39,7 +39,7 @@ const (
             }
         }`
 
-    CommitDataQuery = `
+	CommitDataQuery = `
         query MyQuery($round: String!) {
             commitCs(where: {round: $round}) {
                 round
@@ -51,7 +51,7 @@ const (
             }
         }`
 
-    FulfillRandomnessDataQuery = `
+	FulfillRandomnessDataQuery = `
         query GetFulfillRandomness($round: String!) {
             fulfillRandomnesses(where: {round: $round}) {
                 msgSender
@@ -60,51 +60,55 @@ const (
             }
         }`
 
-    IsOperatorQuery = `
-        query MyQuery($operator: String!) {
-            operatorNumberChangeds(where: {operator: $operator}) {
-                isOperator
+	IsOperatorQuery = `
+        query MyQuery {
+            activatedOperators_collection {
+                operators
+                operatorsCount
+            }
+            activatedOperators(id: "activatedOperators") {
+                operators
+                operatorsCount
             }
         }`
 )
 
 // GetRandomWordsRequestedRequest returns a GraphQL request for fetching random words requested.
 func GetRandomWordsRequestedRequest() *graphql.Request {
-    return graphql.NewRequest(RandomWordsRequestedQuery)
+	return graphql.NewRequest(RandomWordsRequestedQuery)
 }
 
 // GetCommitCsRequest returns a GraphQL request for fetching commitCs data.
 func GetCommitCsRequest(round, msgSender string) *graphql.Request {
-    req := graphql.NewRequest(CommitCsQuery)
-    req.Var("round", round)
-    req.Var("msgSender", msgSender)
-    return req
+	req := graphql.NewRequest(CommitCsQuery)
+	req.Var("round", round)
+	req.Var("msgSender", msgSender)
+	return req
 }
 
 // GetRecoveredDataRequest returns a GraphQL request for fetching recovered data.
 func GetRecoveredDataRequest(round string) *graphql.Request {
-    req := graphql.NewRequest(RecoveredDataQuery)
-    req.Var("round", round)
-    return req
+	req := graphql.NewRequest(RecoveredDataQuery)
+	req.Var("round", round)
+	return req
 }
 
 // GetCommitDataRequest returns a GraphQL request for fetching commit data.
 func GetCommitDataRequest(round string) *graphql.Request {
-    req := graphql.NewRequest(CommitDataQuery)
-    req.Var("round", round)
-    return req
+	req := graphql.NewRequest(CommitDataQuery)
+	req.Var("round", round)
+	return req
 }
 
 // GetFulfillRandomnessDataRequest returns a GraphQL request for fetching fulfill randomness data.
 func GetFulfillRandomnessDataRequest(round string) *graphql.Request {
-    req := graphql.NewRequest(FulfillRandomnessDataQuery)
-    req.Var("round", round)
-    return req
+	req := graphql.NewRequest(FulfillRandomnessDataQuery)
+	req.Var("round", round)
+	return req
 }
 
 // GetIsOperatorRequest returns a GraphQL request for checking if an address is an operator.
-func GetIsOperatorRequest(operator string) *graphql.Request {
-    req := graphql.NewRequest(IsOperatorQuery)
-    req.Var("operator", operator)
-    return req
+func GetIsOperatorRequest() *graphql.Request {
+	req := graphql.NewRequest(IsOperatorQuery)
+	return req
 }
