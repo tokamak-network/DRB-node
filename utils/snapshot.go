bec29bfd89939eb3a6f71e41858cd3d0eff9b544
kaiden <kaiden@tokamak.network>
2025-05-13 10:38:50 +1000

    fix/remove unnecessary error handling

--- utils/snapshot.go
+++ utils/snapshot.go
@@ -1,7 +1,6 @@
 package utils
 
 import (
-	"fmt"
 	"io"
 	"log"
 	"os"
@@ -10,7 +9,7 @@ import (
 
 const snapshotDir = "./snapshots/"
 
-func TakeSnapshot(dst string) error {
+func TakeSnapshot(dst string) {
 	log.Printf("Taking snapshot for %s", dst)
 
 	file, err := os.Open(dst)
@@ -21,7 +20,8 @@ func TakeSnapshot(dst string) error {
 
 	// Create the snapshot directory if it doesn't exist
 	if err := os.MkdirAll(snapshotDir, os.ModePerm); err != nil {
-		return fmt.Errorf("failed to create snapshot directory: %v", err)
+		log.Printf("failed to create snapshot directory: %v", err)
+		return
 	}
 
 	// Create the snapshot file path
@@ -30,43 +30,44 @@ func TakeSnapshot(dst string) error {
 	// Open or create the snapshot file
 	snapshot, err := os.OpenFile(filepath, os.O_CREATE|os.O_WRONLY, 0666)
 	if err != nil {
-		return fmt.Errorf("failed to open/create snapshot file: %v", err)
+		log.Printf("failed to open/create snapshot file: %v", err)
+		return
 	}
 	defer snapshot.Close()
 
 	_, err = file.WriteTo(snapshot)
 	if err != nil {
-		return fmt.Errorf("failed to write file to snapshot: %v", err)
+		log.Printf("failed to write file to snapshot: %v", err)
+		return
 	}
 
 	log.Printf("Succeed to take snapshot of %s", dst)
-
-	return nil
 }
 
-func RevertStates(dst string) error {
+func RevertStates(dst string) {
 	log.Printf("Reverting states from %s", dst)
 
 	filepath := filepath.Join(snapshotDir, dst)
 
 	snapshot, err := os.OpenFile(filepath, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
 	if err != nil {
-		return fmt.Errorf("failed to open snapshot file: %v", err)
+		log.Printf("failed to open snapshot file: %v", err)
+		return
 	}
 	defer snapshot.Close()
 
 	file, err := os.Open(dst)
 	if err != nil {
-		return fmt.Errorf("failed to open leader commit file, %v", err)
+		log.Printf("failed to open leader commit file, %v", err)
+		return
 	}
 	defer file.Close()
 
 	_, err = io.Copy(snapshot, file)
 	if err != nil {
-		return fmt.Errorf("failed to revert states from snapshot: %v", err)
+		log.Printf("failed to revert states from snapshot: %v", err)
+		return
 	}
 
 	log.Printf("%s is reverted successfully", dst)
-
-	return nil
 }
