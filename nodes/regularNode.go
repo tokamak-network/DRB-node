6d843081bdd381ca14bab3ad1165e64341cef0a6
DarkKnight3074 <aryan@tokamak.network>
2025-06-12 17:55:41 +0530

    feat: add the functionality to receive cvs and cos in regular node

--- nodes/regularNode.go
+++ nodes/regularNode.go
@@ -44,6 +44,8 @@ func RunRegularNode(fallbackEthClient *fallback_ethclient.FallbackRPCClient) {
 	h.SetStreamHandler("/sendSecretValue", func(s network.Stream) {
 		regularNode_helper.HandleSecretValueRequest(h, s)
 	})
+	h.SetStreamHandler("/cvsBroadcast", handleCvs)
+	h.SetStreamHandler("/cosBroadcast", handleCos)
 
 	go regularNode_helper.MonitorCommitRequest(fallbackEthClient)
 
@@ -253,7 +255,111 @@ func RunRegularNode(fallbackEthClient *fallback_ethclient.FallbackRPCClient) {
 	}
 }
 
-// sendCOSToLeader sends the COS to the leader node
+func handleCvs(s network.Stream) {
+    defer s.Close()
+
+    var message utils.PeerCommitData
+    if err := json.NewDecoder(s).Decode(&message); err != nil {
+        log.Printf("Failed to decode CVS message: %v", err)
+        return
+    }
+    log.Printf("Received CVS for round %s from EOA %s", message.Round, message.EOAAddress)
+
+    filePath := "peerNodeInfo.json"
+    var peerNodeInfo map[string]utils.PeerCommitData
+
+    if _, err := os.Stat(filePath); err == nil {
+        file, err := os.Open(filePath)
+        if err != nil {
+            log.Printf("Failed to open peerNodeInfo.json: %v", err)
+            return
+        }
+        defer file.Close()
+
+        if err := json.NewDecoder(file).Decode(&peerNodeInfo); err != nil {
+            log.Printf("Failed to decode peerNodeInfo.json: %v", err)
+            return
+        }
+    } else if os.IsNotExist(err) {
+        log.Printf("peerNodeInfo.json does not exist. Creating a new file.")
+        peerNodeInfo = make(map[string]utils.PeerCommitData)
+    } else {
+        log.Printf("Error checking peerNodeInfo.json: %v", err)
+        return
+    }
+
+    key := fmt.Sprintf("%s+%s", message.Round, message.EOAAddress)
+    peerNodeInfo[key] = message
+
+    file, err := os.Create(filePath)
+    if err != nil {
+        log.Printf("Failed to create peerNodeInfo.json: %v", err)
+        return
+    }
+    defer file.Close()
+
+    if err := json.NewEncoder(file).Encode(peerNodeInfo); err != nil {
+        log.Printf("Failed to write to peerNodeInfo.json: %v", err)
+        return
+    }
+
+    log.Printf("Successfully saved CVS data for round %s and EOA %s into peerNodeInfo.json", message.Round, message.EOAAddress)
+}
+
+func handleCos(s network.Stream) {
+    defer s.Close()
+
+    var message utils.PeerCommitData
+
+    if err := json.NewDecoder(s).Decode(&message); err != nil {
+        log.Printf("Failed to decode COS message: %v", err)
+        return
+    }
+
+    log.Printf("Received COS for round %s from EOA %s", message.Round, message.EOAAddress)
+
+    filePath := "peerNodeInfo.json"
+    var peerNodeInfo map[string]utils.PeerCommitData
+
+    if _, err := os.Stat(filePath); err == nil {
+        file, err := os.Open(filePath)
+        if err != nil {
+            log.Printf("Failed to open peerNodeInfo.json: %v", err)
+            return
+        }
+        defer file.Close()
+
+        if err := json.NewDecoder(file).Decode(&peerNodeInfo); err != nil {
+            log.Printf("Failed to decode peerNodeInfo.json: %v", err)
+            return
+        }
+    } else if os.IsNotExist(err) {
+        log.Printf("peerNodeInfo.json does not exist. Creating a new file.")
+        peerNodeInfo = make(map[string]utils.PeerCommitData)
+    } else {
+        log.Printf("Error checking peerNodeInfo.json: %v", err)
+        return
+    }
+
+    key := fmt.Sprintf("%s+%s", message.Round, message.EOAAddress)
+    data := peerNodeInfo[key]
+	data.Cos = message.Cos
+	peerNodeInfo[key] = data
+    file, err := os.Create(filePath)
+    if err != nil {
+        log.Printf("Failed to create peerNodeInfo.json: %v", err)
+        return
+    }
+    defer file.Close()
+
+    if err := json.NewEncoder(file).Encode(peerNodeInfo); err != nil {
+        log.Printf("Failed to write to peerNodeInfo.json: %v", err)
+        return
+    }
+
+    log.Printf("Successfully saved COS data for round %s and EOA %s into peerNodeInfo.json", message.Round, message.EOAAddress)
+}
+
 func sendCosToLeader(ctx context.Context, h core.Host, leaderID peer.ID, commitData utils.CommitData, eoaAddress string, privateKey *ecdsa.PrivateKey) {
 	// Create commit request structure with signed COS and round data
 	req := utils.CosRequest{
